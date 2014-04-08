447820981fde2a685a0d89d2d09fa0227a82f798
hp <minewindows@outlook.com>
2014-04-08 12:19:18 +0800

    三个页面可以布置了...不过好像结构要调整下...否则后面处理布局中的按钮啥的有点乱...呵呵

--- OurApp/app/build/source/r/debug/com/example/android/OurApp/R.java
+++ OurApp/app/build/source/r/debug/com/example/android/OurApp/R.java
@@ -19,9 +19,10 @@ public final class R {
         public static final int unselected=0x7f020005;
     }
     public static final class id {
-        public static final int action_settings=0x7f070002;
-        public static final int action_share=0x7f070001;
-        public static final int pager=0x7f070000;
+        public static final int action_settings=0x7f070003;
+        public static final int action_share=0x7f070002;
+        public static final int button=0x7f070000;
+        public static final int pager=0x7f070001;
     }
     public static final class layout {
         public static final int firstpage=0x7f030000;
--- OurApp/app/src/main/java/com/example/android/OurApp/DummyFragment.java
+++ OurApp/app/src/main/java/com/example/android/OurApp/DummyFragment.java
@@ -1,4 +1,5 @@
 package com.example.android.OurApp;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.Gravity;
@@ -11,26 +12,20 @@ import android.widget.TextView;
 public class DummyFragment extends Fragment
 {
     public static final String ARG_SECTION_NUMBER = "section_number";
+
     // 该方法的返回值就是该Fragment显示的View组件
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState)
     {
-        TextView textView = new TextView(getActivity());
-        textView.setGravity(Gravity.CENTER_HORIZONTAL);
-        // 获取创建该Fragment时传入的参数Bundle
+        View v = null;
         Bundle args = getArguments();
-        // 设置TextView显示的文本
-        textView.setText(args.getInt(ARG_SECTION_NUMBER) + "");
-        textView.setTextSize(40);
-        // 返回该TextView
-
         switch(args.getInt(ARG_SECTION_NUMBER))
         {
-            case 1:break;
-            case 2:break;
-            case 3:break;
+            case 1:v = inflater.inflate(R.layout.firstpage, container, false);break;
+            case 2:v = inflater.inflate(R.layout.secondpage, container, false);break;
+            case 3:v = inflater.inflate(R.layout.thirdpage, container, false);break;
         }
-        return textView;
+        return v;
     }
 }
